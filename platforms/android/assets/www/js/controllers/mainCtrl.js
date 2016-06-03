34e121cc9a41c07393079ff03d601be8fba2a80b
Joakim Annebäck <joakim.anneback@gmail.com>
2016-06-03 15:11:13 +0200

    added time measurement to ui

--- platforms/android/assets/www/js/controllers/mainCtrl.js
+++ platforms/android/assets/www/js/controllers/mainCtrl.js
@@ -1,49 +1,29 @@
 ﻿geoApp.controller('mainCtrl', ['$scope', function ($scope) {
+  document.addEventListener("deviceready", onDeviceReady, false);
+
+  function onDeviceReady() {
+      console.log("navigator.geolocation works well");
+  }
     var end, start;
     var watchID = null;
     //geolocation options timeout is set to x msecs, maximum age y msecs
     var options = {enableHighAccuracy: true, maximumAge: 0, timeout:10000};
 
     $scope.getPos = function () {
+        var start = new Date().getTime();
         if (watchID != null) navigator.geolocation.clearWatch(watchID);
-        start = new Date();
         watchID = navigator.geolocation.getCurrentPosition(function(position) {
-            $scope.position = position;
+            $scope.latitude = position.coords.latitude;
+            $scope.longitude = position.coords.longitude;
+            $scope.timestamp = position.timestamp;
             console.log("got position lat: " + position.coords.latitude + " long: " + position.coords.longitude);
+            var end = new Date().getTime();
+            console.log('Operation took ' + ((end - start) + ' msec'));
+            $scope.time = end - start;
             $scope.$apply();
-        }, function(e) { console.log("Error retrieving position " + e.code + " " + e.message) }, options);
-            end = new Date();
-           	console.log('Operation took ' + (end.getTime() - start.getTime()) + ' msec');
+        }, function(e) {
+          alert("Error retrieving position error code: " + e.code + " error message: " + e.message)
+        }, options);
     };
-    $scope.stop = function(){
-        if(watchID != null){
-            navigator.geolocation.clearWatch(watchID);
-            watchID=null;
-            console.log("stopped watching");
-            alert('Stopped watching');
-        }
-    };
-//    var promise;
-//    $scope.getCurrentPos = function () {
-//        start = new Date();
-//    	navigator.geolocation.getCurrentPosition(function(position) {
-//    		$scope.position =position;
-//    		$scope.$apply();
-//    	},function(e) {console.log("Error retrieving position " + e.code + " " + e.message)}
-//    	, options)
-//    	end = new Date();
-//    	console.log('Operation took ' + (end.getTime() - start.getTime()) + ' msec');
-//    };
-//
-//    $scope.start = function () {
-//        promise = $interval($scope.getCurrentPos, 1000);
-//    };
-//
-//    $scope.stop = function() {
-//        if (angular.isDefined(promise)) {
-//            $interval.cancel(promise);
-//            console.log('stopped interval');
-//            alert('Geolocation Stopped!');
-//        }
-//    };
+
 }]);
